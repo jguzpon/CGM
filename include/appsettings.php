72699d2d5f128a18d8428eb23d771b0bc33ca523
jguzpon <61202532+jguzpon@users.noreply.github.com>
2021-05-14 12:30:58 -0600

    Correccion de errores en Memorando
    Correccion final de Memorando

--- include/appsettings.php
+++ include/appsettings.php
@@ -660,9 +660,9 @@ $suggestAllContent = true;
 $strLastSQL = "";
 $showCustomMarkerOnPrint = false;
 
-$projectBuildKey = "354_1621015358";
+$projectBuildKey = "357_1621015358";
 $wizardBuildKey = "35214";
-$projectBuildNumber = "354";
+$projectBuildNumber = "357";
 
 $mlang_messages = array();
 $mlang_charsets = array();
